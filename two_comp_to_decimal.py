cef6c598ef73cd6034cd6225d422776e5a8c9204
brian98077 <119423295+brian98077@users.noreply.github.com>
2024-10-13 04:07:44 +0800

    Update two_comp_to_decimal.py

--- two_comp_to_decimal.py
+++ two_comp_to_decimal.py
@@ -22,4 +22,4 @@ for i in range(len(num)):
 
 two_comp_num_temp = bin(int(one_comp_num, 2) + 1)[2:]
 two_comp_num = two_comp_num_temp.rjust(TOTAL_WIDTH,'0')
-print("decimal :", two_comp_to_decimal(two_comp_num))
+print("decimal : -",two_comp_to_decimal(two_comp_num))
